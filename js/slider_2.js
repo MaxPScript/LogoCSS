f6d72b1f5af47e50d6138464a4eb7655a681d3d2
Maxim <maxtab1@mail.ru>
2026-04-12 04:01:44 +0300

    Regular

--- js/slider_2.js
+++ js/slider_2.js
@@ -1,37 +1,64 @@
 window.onload = () => {
-	let shift = 100;
 	const tabourets = document.querySelectorAll(".slides.tabouret img");
-	for (let i = 0; i < tabourets.length; i++) {
-		tabourets[i].style.opacity = 0;
-		// tabourets[tabourets.length - 1 - counter].style.opacity = 1;
-		tabourets[tabourets.length - 1].style.opacity = 1;
+	const facades = document.querySelectorAll(".slides.facades img");
+	function setInitialOpacity(images) {
+		for (let i = 0; i < images.length; i++) {
+			images[i].style.opacity = 0;
+			images[images.length - 1].style.opacity = 1;
+		}
 	}
-	console.log(tabourets[0]);
-	let counter = 0;
-	// tabourets[counter].style.opacity = 1;
-	const btnPrev = document.querySelector(".prev");
-	const btnNext = document.querySelector(".next");
-	// console.log(btnPrev, btnNext);
+	setInitialOpacity(tabourets);
+	setInitialOpacity(facades);
+	let counterTaboret = 0;
+	let counterFacade = 0;
+	const btnPrevTabouret = document.querySelector(".prev.tabouret");
+	const btnNextTabouret = document.querySelector(".next.tabouret");
+	const btnPrevFacade = document.querySelector(".prev.facades");
+	const btnNextFacade = document.querySelector(".next.facades");
 	document.addEventListener("click", (ev) => {
-		if (ev.target === btnNext) {
-			counter++;
-			console.log(counter);
-			if (counter === tabourets.length) {
-				counter = tabourets.length - 1;
+		if (ev.target === btnNextTabouret) {
+			counterTaboret++;
+			// console.log(counterTaboret);
+			if (counterTaboret === tabourets.length) {
+				counterTaboret = tabourets.length - 1;
 			}
 			for (let i = 0; i < tabourets.length; i++) {
 				tabourets[i].style.opacity = 0;
-				tabourets[tabourets.length - 1 - counter].style.opacity = 1;
+				tabourets[tabourets.length - 1 - counterTaboret].style.opacity = 1;
 			}
-		} else if (ev.target === btnPrev) {
-			counter--;
-			console.log(counter);
-			if (counter < 0) {
-				counter = 0;
+		} else if (ev.target === btnPrevTabouret) {
+			counterTaboret--;
+			// console.log(counterTaboret);
+			if (counterTaboret < 0) {
+				counterTaboret = 0;
 			}
 			for (let i = 0; i < tabourets.length; i++) {
 				tabourets[i].style.opacity = 0;
-				tabourets[tabourets.length - 1 - counter].style.opacity = 1;
+				tabourets[tabourets.length - 1 - counterTaboret].style.opacity = 1;
+			}
+		}
+	});
+	//
+	document.addEventListener("click", (ev) => {
+		if (ev.target === btnNextFacade) {
+			counterFacade++;
+			// console.log(counterFacade);
+			if (counterFacade === facades.length) {
+				counterFacade = facades.length - 1;
+			}
+			for (let i = 0; i < facades.length; i++) {
+				facades[i].style.opacity = 0;
+				facades[facades.length - 1 - counterFacade].style.opacity = 1;
+			}
+		} else if (ev.target === btnPrevFacade) {
+			counterFacade--;
+			// console.log(counterFacade);
+			if (counterFacade < 0) {
+				counterFacade = 0;
+			}
+			for (let i = 0; i < facades.length; i++) {
+				facades[i].style.opacity = 0;
+				facades[facades.length - 1 - counterFacade].style.opacity = 1;
 			}
 		}
 	});
